fcce7b95f8b8e32e4a8a1b894ee2d02c9606557b
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 19:57:12 +0545

    imp: adding version palceholder

--- src/schema/dbsetting.py
+++ src/schema/dbsetting.py
@@ -2,5 +2,11 @@ from pydantic import BaseModel
 
 
 class Setting(BaseModel):
+    """
+    This schema is used to create 
+     - dbsettings.json 
+    """
     installation_path: str
     debug: bool
+    version:str
+
