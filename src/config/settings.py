571286f147e4ee504d469fbfd897798c94833e56
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 18:24:58 +0545

    add: implemnting settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,14 @@
+"""
+This file is responsible for creating setting.json
+
+"""
+
 
 class IndexDBSetting:
-    def __init__(self) -> None:
+    def __init__(self,installation_path:str,debug:bool) -> None:
+        self.installation_path = installation_path 
+        self.debug = debug
+
+    async def generate_config(self):
         pass
-
-
+    
