6b609171b20fe8713c8418003edaf3dd206e833f
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 18:24:14 +0545

    feat: implementing basic logger config

---
+++ src/logging/logger.py
@@ -0,0 +1,17 @@
+import structlog
+
+def logger_config():
+ """
+ This is a basic logger setup ,
+ we will implement upgrades later-on
+ """
+ structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.add_log_level,
+        structlog.dev.ConsoleRenderer(pad_level=False),
+    ],
+    )
+ return structlog
+
+logger = logger_config().getLogger()
