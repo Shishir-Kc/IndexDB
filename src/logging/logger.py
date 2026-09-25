160d780665ff5b4b8f4d1d676b4d379767cdb531
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 19:46:07 +0545

    imp: making logger config dynamic

--- src/logging/logger.py
+++ src/logging/logger.py
@@ -1,17 +1,19 @@
 import structlog
 
+
 def logger_config():
- """
- This is a basic logger setup ,
- we will implement upgrades later-on
- """
- structlog.configure(
-    processors=[
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.add_log_level,
-        structlog.dev.ConsoleRenderer(pad_level=False),
-    ],
+    """
+    This is a basic logger setup ,
+    we will implement upgrades later-on
+    """
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.add_log_level,
+            structlog.dev.ConsoleRenderer(pad_level=False),
+        ],
     )
- return structlog
+    return structlog
+
 
 logger = logger_config().getLogger()
