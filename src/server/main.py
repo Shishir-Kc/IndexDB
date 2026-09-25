fd5286b31f0b3d11229661bc4825db25e413c945
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-26 05:17:29 +0545

    imp: implementing a test user health endpoint

--- src/server/main.py
+++ src/server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status
 
 from src.logging.logger import logger as log
+from src.server.api.v1 import v1_router
 
 server = FastAPI(title="IndexDB", version="0.0.1", description="A simple DB Engine")
 
@@ -9,3 +10,6 @@ server = FastAPI(title="IndexDB", version="0.0.1", description="A simple DB Engi
 async def health():
     log.info("health")
     return {"status": status.HTTP_200_OK}
+
+
+server.include_router(v1_router, prefix="/api/v1")
