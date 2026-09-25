f8f2a32070b0b7a0216c8054aba72171ab648b32
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 18:24:36 +0545

    test: testing new structlog

--- src/server/main.py
+++ src/server/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, status
+from src.logging.logger import logger as log
 
 server = FastAPI(title="IndexDB", version="0.0.1", description="A simple DB Engine")
 
 
 @server.get("/", tags=["health"])
 async def health():
+    log.info("health")
     return {"status": status.HTTP_200_OK}
