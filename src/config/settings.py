aa49859b30e6ad31d7baadea9eb9af2fc488e9ad
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-25 19:45:38 +0545

    feat: wrapping it into main fucntion for running scripts

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,12 +3,27 @@ This file is responsible for creating setting.json
 
 """
 
+import asyncio
+
+import aiofiles
+
+from src.schema.dbsetting import Setting
+
 
 class IndexDBSetting:
-    def __init__(self,installation_path:str,debug:bool) -> None:
-        self.installation_path = installation_path 
+    def __init__(self, installation_path: str, debug: bool) -> None:
+        self.installation_path = installation_path
         self.debug = debug
 
     async def generate_config(self):
-        pass
-    
+        async with aiofiles.open("dbsetting.json", "w") as file:
+            await file.write(
+                Setting(
+                    installation_path=self.installation_path, debug=self.debug
+                ).model_dump_json(indent=2)
+            )
+
+
+def main():
+    test = IndexDBSetting(installation_path=".config/IndexDB", debug=True)
+    asyncio.run(test.generate_config())
